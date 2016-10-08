99c883af012c26abe3d22ac0fb29708a3ef107e8
Pawel Czop <pawel.czop@gmail.com>
2016-10-08 10:24:23 +0200

    added colant, tidy up the code

--- main.c
+++ main.c
@@ -1,33 +1,4 @@
-/*-------------------------------------------------------------------------
- Example main program for LCD PCD8544 (LCD of Nokia 3310)
 
-
- Copyright (c) 2008, Fandi Gunawan <fandigunawan@gmail.com>
- http://fandigunawan.wordpress.com
-
- This program is free software; you can redistribute it and/or modify it
- under the terms of the GNU General Public License as published by the
- Free Software Foundation; either version 2, or (__at your option) any
- later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
- In other words, you are welcome to use, share and improve this program.
- You are forbidden to forbid anyone else to use, share and improve
- what you give them.   Help stamp out software-hoarding!
-
- -------------------------------------------------------------------------*/
-/*
- Many thanks to Jakub Lasinski for reviewing and for giving
- suggestion to my code
- */
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
